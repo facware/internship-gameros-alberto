c76ed26b5519c164c8ca8599b2d65a256e7417fa
albertogameros <c.alberto.gameros.c@gmail.com>
2018-03-31 19:46:37 -0700

    Se modifica el visor de pdf pero sin resultados de elementos, se regresa a los cambios anteriores y se eliminan lineas de codigo que hacen fallar el sistema

--- src/pages/pdfviewer/pdfviewer.ts
+++ src/pages/pdfviewer/pdfviewer.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
 /*import { FileOpener } from '@ionic-native/file-opener';*/
 /**
  * Generated class for the PdfviewerPage page.
@@ -18,10 +17,18 @@ export class PdfviewerPage {
   url:string;
   constructor(public navCtrl: NavController, public navParams: NavParams  ) {
     this.url ="../../assets/pdf/am.pdf";
+    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PdfviewerPage');
+  }
+  cargarPDF(){
+    //var t=new HTMLDocument;
+    var jf= document.getElementById("pdf");
+    jf.setAttribute("src",this.url);
+    jf.replaceChild.apply(jf);
+
   }
 
   
